f91d6f9d51b135ac0af4105353450b05d66b1e81
HongHyoJu <134509940+HongHyoJu@users.noreply.github.com>
2023-05-30 15:48:01 +0900

    Add files via upload

--- Baekjoon/1차원 배열/HongHyoJu/1546.py
+++ Baekjoon/1차원 배열/HongHyoJu/1546.py
@@ -1,1 +1,9 @@
+N = int(input())
+scores = list(map(int, input().split()))
+M = max(scores)
 
+new_list = []
+for score in scores :
+    new_list.append(score/M *100)
+new_avg = sum(new_list)/N
+print(new_avg)
