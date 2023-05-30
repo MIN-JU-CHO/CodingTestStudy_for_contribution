f550eb136493655e454818bd4fb1690f7fd5667c
Dongyeon Park <yeon0729@sookmyung.ac.kr>
2023-05-30 21:31:27 +0900

    행렬 덧셈

---
+++ Baekjoon/2차원 배열/DyeonPark/2738.py
@@ -0,0 +1,8 @@
+N, M = map(int, input().split())
+mat_A = [list(map(int, input().split())) for _ in range(N)]
+mat_B = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+  for j in range(M):
+    print(mat_A[i][j] + mat_B[i][j], end=' ')
+  print()
