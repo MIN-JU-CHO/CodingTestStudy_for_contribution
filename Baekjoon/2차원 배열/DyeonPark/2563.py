a066e4f45e0f0b351e1466049790e9a26dd97307
Dong-Yeon Park (박동연_T5080) <58678384+DyeonPark@users.noreply.github.com>
2023-05-31 21:57:41 +0900

    색종이

--- Baekjoon/2차원 배열/DyeonPark/2563.py
+++ Baekjoon/2차원 배열/DyeonPark/2563.py
@@ -1,3 +1,4 @@
+### 1st Method ###
 # Data input
 N = int(input())
 paper = [[0 for _ in range(100)] for _ in range(100)]
@@ -10,6 +11,26 @@ for _ in range(N):
   for idx_x in range(10):
     for idx_y in range(10):
       paper[x+idx_x][y+idx_y] = 1
+
+# cal result
+result = 0
+for i in range(100):
+  result += sum(paper[i])
+print(result)
+
+
+### 2nd Method ###
+# Data input
+N = int(input())
+paper = [[0 for _ in range(100)] for _ in range(100)]
+
+for _ in range(N):
+  # set pos
+  x, y = map(int, input().split())
+
+  # coloring with black(1)
+  for idx_x in range(10):
+      paper[x+idx_x][y:y+10] = [1] * 10
 
 # cal result
 result = 0
