975b99042966e9bbd9c5558c18cf0689bab45e87
HongHyoJu <134509940+HongHyoJu@users.noreply.github.com>
2023-06-02 18:56:12 +0900

    소수 구하기

---
+++ Baekjoon/약수, 배수와 소수 2/HongHyoJu/1929.py
@@ -0,0 +1,20 @@
+import math
+
+
+def is_Prime(num):
+    if num == 1:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, int(math.sqrt(num)+1)):
+            if num % i == 0:
+                return False
+        return True
+
+
+M, N = map(int, input().split())
+
+for i in range(M, N+1):
+    if is_Prime(i):
+        print(i)
