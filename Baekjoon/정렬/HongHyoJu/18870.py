4d3a8707b9e1f49e5c07b4a3d37d3231d4aa5635
HongHyoJu <134509940+HongHyoJu@users.noreply.github.com>
2023-06-19 18:07:39 +0900

    좌표 압축

--- Baekjoon/정렬/HongHyoJu/18870.py
+++ Baekjoon/정렬/HongHyoJu/18870.py
@@ -1,1 +1,13 @@
+# list.index(i) 형태의 시간 복잡도 = O(N)
+# { dict[요소] : 요소의 index } 형태의 시간 복잡도 = O(1)
 
+N = int(input())
+arr = list(map(int, input().split()))
+newArr = sorted(set(arr))
+
+dic = {}
+for i in range(len(newArr)):
+    dic[newArr[i]] = i
+
+for i in arr:
+    print(dic[i], end=' ')
