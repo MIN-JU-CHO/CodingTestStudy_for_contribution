1a0d94064cb77d0d9b85d6c65fb0600769cb1d9e
HongHyoJu <134509940+HongHyoJu@users.noreply.github.com>
2023-06-28 17:20:55 +0900

    Add files via upload

--- Baekjoon/스택/HongHyoJu/4949.py
+++ Baekjoon/스택/HongHyoJu/4949.py
@@ -1,1 +1,33 @@
+import sys
+input = sys.stdin.readline
 
+while (True):
+    word = input()
+    stack = []
+
+    if word == '.':
+        break
+
+    for w in word:
+        # if w == '.':
+        #     break
+
+        if w == '(' or w == '[':
+            stack.append(w)
+        elif w == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(w)
+                break
+        elif w == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(w)
+                break
+
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
