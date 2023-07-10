0ac50860f0ccb5e7f71a1cfa2e34c6470bdb065f
HongHyoJu <134509940+HongHyoJu@users.noreply.github.com>
2023-07-10 17:11:43 +0900

    별 찍기

--- Baekjoon/재귀/HongHyoJu/2447.py
+++ Baekjoon/재귀/HongHyoJu/2447.py
@@ -1,1 +1,29 @@
+def draw(n):
+    if n == 3:
+        star[0][:3] = star[2][:3] = [1]*3
+        star[1][:3] = [1, 0, 1]
+        return
 
+    a = n//3
+    draw(a)
+
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                continue
+            for k in range(a):
+                star[a*i+k][a*j:a*(j+1)] = star[k][:a]
+
+
+N = int(input())
+star = [[0] * N for i in range(N)]
+
+draw(N)
+
+for i in star:
+    for j in i:
+        if j:
+            print("*", end='')
+        else:
+            print(" ", end='')
+    print()
