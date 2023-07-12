041a27022239ed91e0bc510d6638d6509fb895d3
Dong-Yeon Park (박동연_T5080) <58678384+DyeonPark@users.noreply.github.com>
2023-07-12 19:41:09 +0900

    하노이 탑 이동 순서

---
+++ Baekjoon/재귀/DyeonPark/11729.py
@@ -0,0 +1,17 @@
+N = int(input())
+print(2 ** N - 1)
+
+A, B, C = [], [], []
+
+def move(N, _from, _to):
+  print(f{_from} {_to}")
+
+def hanoi(N, _from, _to, _via):
+  if N == 1:
+    move(N, _from, _to)
+  else:
+    hanoi(N-1, _from, _via, _to)
+    move(N, _from, _to)
+    hanoi(N-1, _via, _to, _from)
+
+hanoi(N, "A", "C", "B")
