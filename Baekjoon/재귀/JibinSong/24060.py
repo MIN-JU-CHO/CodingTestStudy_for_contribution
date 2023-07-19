85daef9baa43febe9260a5b143748b9ea0a6ca79
jibin86 <89712324+jibin86@users.noreply.github.com>
2023-07-19 22:15:55 +0900

    Update 24060.py

--- Baekjoon/재귀/JibinSong/24060.py
+++ Baekjoon/재귀/JibinSong/24060.py
@@ -1,44 +1,49 @@
-def merge_sort(array, p, r):
-  print('first', p, r)
-  if r-p >= 2:
-    q = (p + r) // 2
+def merge_sort(array):
+  if len(array)>=2:
+    q = (0 + len(array)-1) // 2
     # 전반부 정렬
-    merge_sort(array, p, q)
+    front = array[:q + 1]
+    print('front',front)
+    front = merge_sort(front)
     # 후반부 정렬
-    merge_sort(array, q + 1, r)
+    back = array[q + 1:]
+    print('back',back)
+    back = merge_sort(back)
     # 병합
-    merge(array, p, q, r)
+    print("start merge")
+    print(front, back)
+    return merge(front, back)
+  else:
+    return array
 
 
-def merge(array, p, q, r):
-  global cnt
-  i = p
-  j = q + 1
+def merge(front, back):
+  i = 0
+  j = 0
   temp = []
-  while i <= q and j <= r:
-    if array[i] <= array[j]:
-      temp.append(array[i])
-      print("temp", temp)
+  while i < len(front) and j < len(back):
+    if front[i] <= back[j]:
+      temp.append(front[i])
       i += 1
     else:
-      temp.append(array[j])
-      # print("temp", temp)
+      temp.append(back[j])
       j += 1
   # 왼쪽 배열이 남은 경우
-  while i <= q:
-    temp.extend(array[i:])
-    # print("temp", temp)
+  while i < len(front):
+    temp.extend(front[i:])
+    result.extend(temp)  # 저장된 원소 리스트
+    return temp
   # 오른쪽 배열이 남은 경우
-  while j <= r:
-    temp.extend(array[j:])
-    # print("temp", temp)
+  while j < len(back):
+    temp.extend(back[j:])
+    result.extend(temp)  # 저장된 원소 리스트
+    return temp
 
-  array[p:r] = temp
-  result.append(temp)  # 저장된 원소 리스트
-
-
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 array = list(map(int, input().split()))
 result = []
-merge_sort(array, p=0, r=len(array) - 1)
-print(result)
+merge_sort(array)
+try:
+  print(result[k-1])
+except:
+  print(-1)
