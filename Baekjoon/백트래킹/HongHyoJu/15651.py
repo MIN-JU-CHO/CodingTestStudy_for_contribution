ec9aa83efc8f94fd7570cf770810b5a41fd47527
HongHyoJu <134509940+HongHyoJu@users.noreply.github.com>
2023-07-21 17:58:31 +0900

    Add files via upload

--- Baekjoon/백트래킹/HongHyoJu/15651.py
+++ Baekjoon/백트래킹/HongHyoJu/15651.py
@@ -4,17 +4,12 @@ def dfs():
         return
 
     for i in range(1, n+1):
-        if visited[i]:
-            continue
-        visited[i] = True
         s.append(i)
         dfs()
         s.pop()
-        visited[i] = False
 
 
 n, m = map(int, input().split())
 s = []
-visited = [False]*(n+1)
 
 dfs()
---
+++ Baekjoon/백트래킹/HongHyoJu/15652.py
@@ -0,0 +1,15 @@
+def dfs(x):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+
+    for i in range(x, n+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+
+
+n, m = map(int, input().split())
+s = []
+
+dfs(1)
