416055903ca52855491743ca42135922311d4094
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2023-07-31 21:01:00 +0900

    연산자 끼워넣기 - dfs

--- Baekjoon/백트래킹/MinjuCho/14888.py
+++ Baekjoon/백트래킹/MinjuCho/14888.py
@@ -1,48 +1,33 @@
 from itertools import permutations
 import sys
-import math
 input = sys.stdin.readline
 N = int(input())
 nums = list(map(int, input().split()))
 num_opers = list(map(int, input().split()))
 
-# 각 수만큼 oper 담기
-operators = []
-for i in range(4):
-    if i==0:
-        operators.extend('+' * num_opers[i])
-    elif i==1:
-        operators.extend('-' * num_opers[i])
-    elif i==2:
-        operators.extend('*' * num_opers[i])
-    elif i==3:
-        operators.extend('/' * num_opers[i])
-# 모든 순열 경우 구하기
-combs_opers = list(permutations(operators, N-1))
-
-# 모든 경우 값 구해, 최소 최대 구하기
 maximum = int(-1e9)
 minimum = int(1e9)
-for opers in combs_opers:
-    temp = nums[0]
-    #print(temp, end = ' ')
-    for i in range(1, N):
-        if opers[i-1] == '+':
-            temp += nums[i]
-            #print('+', nums[i], end = ' ')
-        elif opers[i-1] == '-':
-            temp -= nums[i]
-            #print('-', nums[i], end = ' ')
-        elif opers[i-1] == '*':
-            temp *= nums[i]
-            #print('*', nums[i], end = ' ')
-        elif opers[i-1] == '/':
-            temp = int(temp/nums[i])
-            #print('/', nums[i], end = ' ')
-    #print(' =', temp)
-    if temp > maximum:
-        maximum = temp
-    if temp < minimum:
-        minimum = temp
+
+def operate(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    # 하나의 경우의 수 연산 끝마침
+    if depth == N:
+        if total > maximum:
+            maximum = total
+        if total < minimum:
+            minimum = total
+        return
+    # 연산자 수 1 이상일 때 다 해보기
+    if plus:
+        operate(depth+1, total+nums[depth], plus-1, minus, multiply, divide)
+    if minus:
+        operate(depth+1, total-nums[depth], plus, minus-1, multiply, divide)
+    if multiply:
+        operate(depth+1, total*nums[depth], plus, minus, multiply-1, divide)
+    if divide:
+        operate(depth+1, int(total/nums[depth]), plus, minus, multiply, divide-1)
+
+operate(1, nums[0], num_opers[0], num_opers[1], num_opers[2], num_opers[3])
+
 print(maximum)
 print(minimum)
