9671cfc5f0406a5561c69e41d8e5b609c0c9b152
HongHyoJu <134509940+HongHyoJu@users.noreply.github.com>
2023-08-14 23:18:49 +0900

    RGB거리

--- Baekjoon/동적 계획법1/HongHyoJu/1149.py
+++ Baekjoon/동적 계획법1/HongHyoJu/1149.py
@@ -1,1 +1,10 @@
+N = int(input())
+# 빨강, 초록, 파랑
+array = [list(map(int, input().split())) for _ in range(N)]
 
+for i in range(1, N):
+    array[i][0] = min(array[i - 1][1], array[i - 1][2]) + array[i][0]
+    array[i][1] = min(array[i - 1][0], array[i - 1][2]) + array[i][1]
+    array[i][2] = min(array[i - 1][0], array[i - 1][1]) + array[i][2]
+
+print(min(array[N - 1]))
