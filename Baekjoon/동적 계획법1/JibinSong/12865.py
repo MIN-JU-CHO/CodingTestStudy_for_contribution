f138d4f905def5e7bdb36a92eaf2a3269452217f
jibin86 <89712324+jibin86@users.noreply.github.com>
2023-08-30 21:01:28 +0900

    Create 12865.py

---
+++ Baekjoon/동적 계획법1/JibinSong/12865.py
@@ -0,0 +1,14 @@
+'''
+참고 레퍼런스: https://myjamong.tistory.com/319
+'''
+
+n, k = map(int, input().split())
+
+# 무게, 가치
+dp = [0] * (k + 1)
+
+for _ in range(n):
+  w, v = map(int, input().split())
+  for j in range(k, w - 1, -1):
+    dp[j] = max(dp[j], dp[j - w] + v)
+print(max(dp))
