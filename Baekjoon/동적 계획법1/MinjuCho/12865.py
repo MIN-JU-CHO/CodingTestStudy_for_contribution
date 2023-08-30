e4e476c7175328e63e6462332d95d03d0704949c
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2023-08-30 17:45:51 +0900

    평범한 배낭

--- Baekjoon/동적 계획법1/MinjuCho/12865.py
+++ Baekjoon/동적 계획법1/MinjuCho/12865.py
@@ -1,7 +1,10 @@
+# https://velog.io/@cuppizza/백준-12865-평범한-배낭-파이썬
+
 import sys
 input = sys.stdin.readline
 N, K = map(int, input().split())
 dp = [0] * (K+1)
+
 for _ in range(N):
     w, v = map(int, input().split())
     # 더 큰 weight 입력 예외처리
@@ -11,8 +14,8 @@ for _ in range(N):
     for j in range(K, 0, -1):
         if j+w<=K and dp[j] != 0:
             # weight 확인, dp값 존재하면 -> 해당 자리와 새 값 비교
-            #print("j+w=",j,'+',w,"=",j+w, ", dp[j+w] =", dp[j+w],  ", dp[j] =", dp[j],"v=", v)
             dp[j+w] = max(dp[j+w], dp[j]+v)
     # 자기 자리 max update
     dp[w] = max(dp[w], v)
+    
 print(max(dp))
