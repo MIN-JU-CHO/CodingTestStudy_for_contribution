2711438fa3b685aadb78ded77b38559815d34c49
Dong-Yeon Park (박동연_T5080) <58678384+DyeonPark@users.noreply.github.com>
2023-10-04 17:18:20 +0900

    쿼드트리: 풀이 이미지 링크 수정

--- Baekjoon/분할 정복/DyeonPark/1992.py
+++ Baekjoon/분할 정복/DyeonPark/1992.py
@@ -1,5 +1,5 @@
 # 소요 시간: 약 30m
-# 풀이 링크: [이미지](https://file.notion.so/f/f/e3a1bb69-1ac8-40f7-baa4-cd9b87eabde9/ee76ec69-e11a-4e0e-9c93-a68976901ea5/%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%89%E1%85%B3%E1%86%B8%E1%84%8C%E1%85%A1%E1%86%BC_-16.jpg?id=147d7c12-c6a3-4413-b846-2e38c2178801&table=block&spaceId=e3a1bb69-1ac8-40f7-baa4-cd9b87eabde9&expirationTimestamp=1696550400000&signature=IQNfjrf5CgxMqpMG0WOZSztvpM6TFj_OXKAFKi1WcBM&downloadName=%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%89%E1%85%B3%E1%86%B8%E1%84%8C%E1%85%A1%E1%86%BC+%F0%9F%98%8E-16.jpg) 
+# 풀이 링크: https://file.notion.so/f/f/e3a1bb69-1ac8-40f7-baa4-cd9b87eabde9/ee76ec69-e11a-4e0e-9c93-a68976901ea5/%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%89%E1%85%B3%E1%86%B8%E1%84%8C%E1%85%A1%E1%86%BC_-16.jpg?id=147d7c12-c6a3-4413-b846-2e38c2178801&table=block&spaceId=e3a1bb69-1ac8-40f7-baa4-cd9b87eabde9&expirationTimestamp=1696550400000&signature=IQNfjrf5CgxMqpMG0WOZSztvpM6TFj_OXKAFKi1WcBM&downloadName=%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%89%E1%85%B3%E1%86%B8%E1%84%8C%E1%85%A1%E1%86%BC+%F0%9F%98%8E-16.jpg
 # 참고 여부: 참고하지 않음
 import sys
 sys.setrecursionlimit(1000000)
